e2a3bdd6384fccef257873363f21e02c1b925fe6
Eddod <edwinwesterberg44@gmail.com>
2022-04-25 17:50:24 +0200

    init

---
+++ Js/index.js
@@ -0,0 +1,29 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('nav__link');
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');})
+})
+
+var coll = document.getElementsByClassName("btn");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var textcontent = document.getElementById("#textcontent");
+    textcontent.hidden = !textcontent.hidden;
+    console.log(textcontent)
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
